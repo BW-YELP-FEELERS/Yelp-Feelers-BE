0bf761d51f57aeae89e83e3b4962d429066718f1
greenhornsr <srollins11279@gmail.com>
2019-10-19 20:42:14 -0700

    initial commit, initial file structure built out, server set up on port 8015

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// create express server
+const server = express();
+
+//middleware
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("WE ARE UP AND RUNNING!!")
+})
+
+
+
+module.exports = server;
