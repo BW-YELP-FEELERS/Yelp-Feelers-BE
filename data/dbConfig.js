313f198f549b10c42b30aa8a8e9a3fcb0a2d3a6b
greenhornsr <srollins11279@gmail.com>
2019-10-19 21:05:00 -0700

    knex initialized to add knexfile.js, added dbConfig.js; connected to knexfile

---
+++ data/dbConfig.js
@@ -0,0 +1,5 @@
+const knex = require('knex');   // knex library
+const knexConfig = require('../knexfile');   // knexfile database connector
+const environment = process.env.DB_ENV || "development"
+
+module.exports = knex(knexConfig[environment]);
