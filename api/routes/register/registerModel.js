e9d5ba117cecf4e79ff4b9ee86059b303e71a925
greenhornsr <srollins11279@gmail.com>
2019-10-21 12:17:01 -0700

    adjusting model for register

--- api/routes/register/registerModel.js
+++ api/routes/register/registerModel.js
@@ -1,23 +1,24 @@
 const db = require('../../../data/dbConfig');
 const user = require('../users/usersModel');
 
+module.exports = (userCreds) => {
+    const {username, password} = userCreds
+    return db('yfusers')
+    .insert({username, password})
+    .then(([id]) => {
+        return db('yfusers')
+        .select('id', 'username')
+        .where({id})
+        .first()
+    })
+}
+
 // module.exports = (userCreds) => {
 //     return db('yfusers')
 //     .insert(userCreds)
 //     .then(([id]) => {
 //         return db('yfusers')
-//         .select('id', 'username')
 //         .where({id})
-//         .first()
+//         .first()  
 //     })
 // }
-
-module.exports = (userCreds) => {
-    return db('yfusers')
-    .insert(userCreds)
-    .then(([id]) => {
-        return db('yfusers')
-        .where({id})
-        .first()  
-    })
-}
