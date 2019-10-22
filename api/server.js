4a1988deacca80ef3324a2228c3fbdaddd44570c
greenhornsr <srollins11279@gmail.com>
2019-10-22 14:32:30 -0700

    added protected routes for retrieving yelpfeelers modified review data

--- api/server.js
+++ api/server.js
@@ -12,9 +12,13 @@ const loginRoutes = require('./routes/login/loginRouter');
 const usersRoutes = require('./routes/users/usersRouter');
 const mockrevsRoutes = require('./routes/mockreviews/mockrevsRouter');
 
+//import mw 
+const authenticate = require('../auth/auth');
+
 //middleware
 server.use(helmet(), cors(), express.json());
 server.use('/', registerRoutes, loginRoutes, usersRoutes, mockrevsRoutes)
+server.use('/auth', authenticate, mockrevsRoutes)
 
 server.get('/', (req, res) => {
     res.send(`WE ARE UP AND RUNNING!!`)
--- auth/auth.js
+++ auth/auth.js
@@ -6,10 +6,11 @@ const jwtKey = process.env.JWT_SECRET || 'i swear I added a secret.  I just know
 
 module.exports = (req, res, next) => {
     const mytoken = req.get('Authorization');
-    return mytoken ? jwt.verify(mytoken, jwtKey, (error, decodedmytoken) => {
+    return mytoken ? jwt.verify(mytoken, jwtKey, (err, decodedmytoken) => {
         return err ? 
         (res.status(401).json(err)):
-        (req.decoded = decodedmytoken) (next())  
+        (req.decoded = decodedmytoken),
+        (next())  
     }):
     res.status(401).json({error: 'Must be logged in with permissions'})
 }
