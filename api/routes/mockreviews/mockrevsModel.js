2effbe931e0a94d0ffc6a873014111a50bf7054e
greenhornsr <srollins11279@gmail.com>
2019-10-22 17:33:54 -0700

    changed unprotected reviews model to live data with 5k records

--- api/routes/mockreviews/mockrevsModel.js
+++ api/routes/mockreviews/mockrevsModel.js
@@ -6,12 +6,12 @@ module.exports = {
 }
 
 function find(){
-    return db('mockreviews')
+    return db('livereviews')
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
 }
 
 function findbyid(id){
-    return db('mockreviews')
+    return db('livereviews')
     .select('business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
     .where({id})
     .first()
--- api/routes/protectedmockreviews/protmockrevsModel.js
+++ api/routes/protectedmockreviews/protmockrevsModel.js
@@ -6,12 +6,12 @@ module.exports = {
 }
 
 function find(){
-    return db('livereviews')
+    return db('mockreviews')
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'adjusted_store_rating as yfmodified_store_rating', 'original_yelp_user_rating as user_review', 'adjusted_sentiment_rating as yfmodified_user_rating',  'original_text_review')
 }
 
 function findbyid(id){
-    return db('livereviews')
+    return db('mockreviews')
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'adjusted_store_rating as yfmodified_store_rating', 'original_yelp_user_rating as user_review', 'adjusted_sentiment_rating as yfmodified_user_rating',  'original_text_review')
     .where({id})
     .first()
