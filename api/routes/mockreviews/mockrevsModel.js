a66f54827c5f75ebaad8ae14195c8923c8f434e4
greenhornsr <srollins11279@gmail.com>
2019-10-22 18:11:35 -0700

    removed offset

--- api/routes/mockreviews/mockrevsModel.js
+++ api/routes/mockreviews/mockrevsModel.js
@@ -7,7 +7,6 @@ module.exports = {
 
 function find(){
     return db('livereviews')
-    .offset(10)
     .limit(20)
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
 }
