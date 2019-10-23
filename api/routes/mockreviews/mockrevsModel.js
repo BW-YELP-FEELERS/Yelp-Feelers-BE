eac2a17515df7eae210240303d727b2f865d6799
greenhornsr <srollins11279@gmail.com>
2019-10-22 18:21:05 -0700

    corrected orderby to orderBy

--- api/routes/mockreviews/mockrevsModel.js
+++ api/routes/mockreviews/mockrevsModel.js
@@ -9,7 +9,7 @@ function find(){
     return db('livereviews')
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
     .limit(20)
-    .orderby(`original_yelp_user_rating`, 'desc')
+    .orderBy(`original_yelp_user_rating`, 'desc')
 }
 
 function findbyid(id){
