ce78cd23730cc57b4551febd28d387e5250d7fb4
greenhornsr <srollins11279@gmail.com>
2019-10-22 18:08:35 -0700

    updated limits restrictions for both review models

--- api/routes/mockreviews/mockrevsModel.js
+++ api/routes/mockreviews/mockrevsModel.js
@@ -7,14 +7,14 @@ module.exports = {
 
 function find(){
     return db('livereviews')
-    // .offset(10)
-    .limit(10)
+    .offset(10)
+    .limit(20)
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
 }
 
 function findbyid(id){
     return db('livereviews')
-    .select('business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
+    .select('id', 'business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
     .where({id})
     .first()
 }
--- api/routes/protectedmockreviews/protmockrevsModel.js
+++ api/routes/protectedmockreviews/protmockrevsModel.js
@@ -7,8 +7,8 @@ module.exports = {
 
 function find(){
     return db('livereviews')
-    // .offset(10)
-    .limit(10)
+    .offset(10)
+    .limit(20)
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'adjusted_store_rating as yfmodified_store_rating', 'original_yelp_user_rating as user_review', 'adjusted_sentiment_rating as yfmodified_user_rating',  'original_text_review')
 }
 
