0149894954e209488504aa09a1310e861b620368
greenhornsr <srollins11279@gmail.com>
2019-10-22 17:41:32 -0700

    added offset and limit to find methods in mockrev models, protected and unprotected

--- api/routes/protectedmockreviews/protmockrevsModel.js
+++ api/routes/protectedmockreviews/protmockrevsModel.js
@@ -6,12 +6,14 @@ module.exports = {
 }
 
 function find(){
-    return db('mockreviews')
+    return db('livereviews')
+    .offset(10)
+    .limit(10)
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'adjusted_store_rating as yfmodified_store_rating', 'original_yelp_user_rating as user_review', 'adjusted_sentiment_rating as yfmodified_user_rating',  'original_text_review')
 }
 
 function findbyid(id){
-    return db('mockreviews')
+    return db('livereviews')
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'adjusted_store_rating as yfmodified_store_rating', 'original_yelp_user_rating as user_review', 'adjusted_sentiment_rating as yfmodified_user_rating',  'original_text_review')
     .where({id})
     .first()
