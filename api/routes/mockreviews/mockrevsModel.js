c795ee1dc36a94dd57655e90594d05fd82adc0ea
greenhornsr <srollins11279@gmail.com>
2019-10-22 19:16:35 -0700

    modified orderBy in mockrevsModel.

--- api/routes/mockreviews/mockrevsModel.js
+++ api/routes/mockreviews/mockrevsModel.js
@@ -8,8 +8,8 @@ module.exports = {
 function find(){
     return db('livereviews')
     .select('id','business_id', 'business_name', 'address', 'city', 'state', 'postal_code as zip', 'categories', 'yelp_store_rating as yelp_rating', 'original_yelp_user_rating as user_review', 'original_text_review')
+    .orderBy(`user_review`, 'desc')
     .limit(20)
-    .orderBy(`original_yelp_user_rating`, 'desc')
 }
 
 function findbyid(id){
