efd783ed6b93d822fd91b64f5e0e05a294c05a94
greenhornsr <srollins11279@gmail.com>
2019-10-23 13:40:16 -0700

    added comment to migration file

--- data/migrations/20191022200206_create-favorites-fk-table.js
+++ data/migrations/20191022200206_create-favorites-fk-table.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex) {
     knex.schema.createTable('favorites', function (tbl) {
-        tbl.foreign('user_id').references('id').inTable('yfusers');
-        tbl.foreign('review_id').references('id').inTable('livereviews');
+        tbl.foreign('user_id').references('id').inTable('yfusers').comment('This is the users Foreign Key');
+        tbl.foreign('review_id').references('id').inTable('livereviews').comment('This is the review Foreign Key');
     });
 };
 
