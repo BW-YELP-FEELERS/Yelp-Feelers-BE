69b59f7411dbc01f1456f253e2a3aa6393c4cbf6
greenhornsr <srollins11279@gmail.com>
2019-10-23 18:30:02 -0700

    added basic layout to favorites model

---
+++ api/routes/favorites/favoritesModel.js
@@ -0,0 +1,28 @@
+const db = require('../../../data/dbConfig');  // connect to db via dbConfig which is connected to knexfile.
+
+module.exports = {
+    addFavorite,
+    getFavorite,
+    getFavoriteById,
+    removeFavorite
+}
+
+// add a favorite
+function addFavorite(){
+    return db('')
+}
+
+// get favorites
+function getFavorite(){
+    return null
+}
+
+// get a favorite by id
+function getFavoriteById(){
+    return null
+}
+
+// remove a favorite
+const addFavorite = () => {
+    return null
+}
